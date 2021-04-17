bae31afc7221e2adba00b45b8293a3656f22d557
Andrew8bit <Andrewbith@gmail.com>
2021-04-17 00:50:57 -0700

    need to work on transition from splash page to homepage

--- src/components/pages/Splash.js
+++ src/components/pages/Splash.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
 
 export default function Splash() {
-    return (
-        <div className="splash-page">
-            <p> andrew bith</p>
-        </div>
-    )
+  return (
+    <div className="splash-page">
+      <div className="light-div">
+        <button className="name-flicker">
+          <span className="I">A</span>
+          <span className="L">N</span>
+          <span className="I">D</span>
+          <span className="I">R</span>
+          <span className="H">E</span>
+          <span className="I">W</span>
+          <span>
+            <span className="I">8</span>
+          </span>
+          <span className="H">B</span>
+          <span className="H">I</span>
+          <span className="I">T</span>
+        </button>
+      </div>
+      <br/>
+      <div className="typewriter">
+          <ul className="dynamic-txt">
+              <li><span className="teal flicker-txt">{"{Developer}"}</span></li>
+              <li><span className="orange flicker-txt">{"{Programmer}"}</span></li>
+              <li><span className="yellow flicker-txt">{"{Engineer}"}</span></li>
+              <li><span className="violet flicker-txt">{"{Student}"}</span></li>
+          </ul>
+      </div>
+      <br />
+      <button className="light-fix">
+          <p>Fix the lights </p>
+      </button>
+    </div>
+  );
 }
